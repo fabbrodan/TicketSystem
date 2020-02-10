7b5f83c3f20cd2fdcb3777bb954a4f06841228e5
Daniel Åslund <daniel.aaslund@gmail.com>
2020-02-10 23:30:09 +0100

    Changed Index operations to async

--- TicketSystemAPI/TicketSystemAPI/Controllers/IndexController.cs
+++ TicketSystemAPI/TicketSystemAPI/Controllers/IndexController.cs
@@ -20,24 +20,27 @@ namespace TicketSystemAPI.Controllers
         private readonly ElasticClient _client;
         private readonly IOptions<DbOptions> _dbOptions;
 
-        public IndexController(IOptions<ElasticOptions> options, IOptions<DbOptions> dbOptions)
+        public IndexController(IOptions<ElasticOptions> elasticOptions, IOptions<DbOptions> dbOptions)
         {
-            _client = new ElasticClient(new ConnectionSettings(new Uri(options.Value.ClusterUrl)));
+            _client = new ElasticClient(new ConnectionSettings(new Uri(elasticOptions.Value.ClusterUrl)));
             _dbOptions = dbOptions;
         }
 
         [HttpPost]
         [Route("Customers")]
-        public void IndexCustomers()
+        public async void IndexCustomers()
         {
+            await _client.Indices.DeleteAsync("customers");
+
             using (SqlConnection conn = new SqlConnection(_dbOptions.Value.ConnectionString))
             {
                 try
                 {
                     conn.Open();
-                    _client.Bulk(request => request
+                    var customers = await conn.QueryAsync<Customers>("SELECT * FROM Customers;");
+                    await _client.BulkAsync(request => request
                     .Index("customers")
-                    .IndexMany<Customers>(conn.Query<Customers>("SELECT * from Customers;")));
+                    .IndexMany<Customers>(customers));
                 }
                 catch (SqlException exc)
                 {
@@ -48,16 +51,19 @@ namespace TicketSystemAPI.Controllers
 
         [HttpPost]
         [Route("Concerts")]
-        public void IndexConcerts()
+        public async void IndexConcerts()
         {
+            await _client.Indices.DeleteAsync("concerts");
+
             using (SqlConnection conn = new SqlConnection(_dbOptions.Value.ConnectionString))
             {
                 try
                 {
                     conn.Open();
-                    _client.Bulk(request => request
+                    var concerts = await conn.QueryAsync<Concerts>("SELECT * FROM Concerts;");
+                    await _client.BulkAsync(request => request
                     .Index("concerts")
-                    .IndexMany<Concerts>(conn.Query<Concerts>("SELECT * FROM Concerts;")));
+                    .IndexMany<Concerts>(concerts));
                 }
                 catch (SqlException exc)
                 {
@@ -65,5 +71,85 @@ namespace TicketSystemAPI.Controllers
                 }
             }
         }
+
+        [HttpPost]
+        [Route("Venues")]
+        public async void IndexVenues()
+        {
+            await _client.Indices.DeleteAsync("venues");
+
+            using (SqlConnection conn = new SqlConnection(_dbOptions.Value.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    var venues = await conn.QueryAsync<Venues>("SELECT * FROM Venues;");
+                    await _client.BulkAsync(request => request
+                    .Index("venues")
+                    .IndexMany<Venues>(venues));
+                }
+                catch(SqlException exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+            }
+        }
+
+        [HttpPost]
+        [Route("Artists")]
+        public async void IndexArtists()
+        {
+            await _client.Indices.DeleteAsync("artists");
+
+            using (SqlConnection conn = new SqlConnection(_dbOptions.Value.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    var artists = await conn.QueryAsync<Artists>("SELECT * FROM Artists;");
+                    await _client.BulkAsync(request => request
+                    .Index("artists")
+                    .IndexMany<Artists>(artists));
+                }
+                catch (SqlException exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+            }
+        }
+
+        /*
+        [HttpPost]
+        [Route("Index")]
+        public async void Index()
+        {
+            using (SqlConnection conn = new SqlConnection(_dbOptions.Value.ConnectionString))
+            {
+                try
+                {
+                    string sql = @"SELECT c.CalendarDate, c.Cancelled, c.Price, c.TicketsLeft, a.ArtistName, v.VenueName, v.Capacity, v.Coordinates
+                                FROM Concerts c
+                                INNER JOIN Artists a ON c.ArtistId = c.ArtistId
+                                INNER JOIN Venues v ON c.VenueId = v.VenueId";
+                    conn.Open();
+                    IEnumerable<dynamic>indexPOCOs = await conn.QueryAsync(sql);
+
+                    BulkDescriptor descriptor = new BulkDescriptor();
+
+                    foreach (var doc in indexPOCOs)
+                    {
+                        descriptor.Index<object>(i => i
+                            .Index("mainIndex")
+                            .Document(doc));
+                    }
+
+                    await _client.BulkAsync(descriptor);
+                }
+                catch (SqlException exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+            }
+        }*/
     }
 }
---
+++ TicketSystemAPI/TicketSystemAPI/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using TicketSystemAPI.Utils;
+using TicketSystemAPI.Models;
+using Dapper;
+using Nest;
+using Microsoft.Extensions.Options;
+
+namespace TicketSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly ElasticClient _client;
+        private readonly IOptions<DbOptions> _dbOptions;
+
+        public SearchController(IOptions<DbOptions> dbOptions, IOptions<ElasticOptions> elasticOptions)
+        {
+            _client = new ElasticClient(new ConnectionSettings(new Uri(elasticOptions.Value.ClusterUrl)));
+            _dbOptions = dbOptions;
+        }
+    }
+}
