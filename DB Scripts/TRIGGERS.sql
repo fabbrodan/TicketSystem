e165be69187d73b8fe51546aad06085a9990f990
Daniel Åslund <daniel.aaslund@gmail.com>
2020-02-10 11:43:46 +0100

    Added trigger for cancel concert operations

--- DB Scripts/TRIGGERS.sql
+++ DB Scripts/TRIGGERS.sql
@@ -11,3 +11,18 @@ INNER JOIN inserted i ON i.VenueId = v.VenueId);
 
 END
 GO
+
+CREATE TRIGGER [TR_Cancel_Concert] ON [Concerts]
+AFTER UPDATE
+AS
+BEGIN
+IF UPDATE(Cancelled)
+BEGIN
+INSERT INTO Coupons
+(TicketId)
+SELECT t.TicketId
+FROM Tickets t
+INNER JOIN inserted i ON t.ConcertId = i.ConcertId
+END
+END
+GO
